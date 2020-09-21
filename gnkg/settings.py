5a6c04101d49a79d62c5a78e1cbd4f7ef90ec93a
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2020-09-21 15:54:13 -0500

    Add postgresql for heroku

--- gnkg/settings.py
+++ gnkg/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,5 @@ CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "America/Chicago"
+
+django_heroku.settings(locals())
