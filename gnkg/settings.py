2f80e58547c0bc27788e16b286f7c19c6ba0bd05
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2020-09-21 21:49:40 -0500

    add database conn

--- gnkg/settings.py
+++ gnkg/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,6 +84,8 @@ WSGI_APPLICATION = "gnkg.wsgi.application"
 
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
